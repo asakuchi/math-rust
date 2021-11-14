ddc98693b5412e2290b4d99f75183332647036cb
asakuchi <paleApricot.asakuchi@gmail.com>
2021-11-14 21:47:54 +0900

    add gcd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::mem;
+
 fn main() {
-    println!("Hello, world!");
+    println!("gcd({}, {}) = {}", 1071, 1029, gcd(1071, 1029));
+    println!("gcd({}, {}) = {}", 12, 30, gcd(12, 30));
+}
+
+fn gcd(mut a: i32, mut b: i32) -> i32 {
+    if b > a {
+        mem::swap(&mut a, &mut b);
+    }
+
+    while b != 0 {
+        let old_b = b;
+        b = a % b;
+        a = old_b;
+    }
+
+    a
 }
